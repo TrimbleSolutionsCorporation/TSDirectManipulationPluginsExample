1dff52179b5f76268012d3d9973a24fdcbc80d02
cahe <carlos.herrero@trimble.com>
2020-05-22 14:59:58 +0300

    Added examples for prototype of TeklaStructures 2020

---
+++ DirectManipulationPlugins.Examples_2020/BasicPlugin/BeamPlugin.cs
@@ -0,0 +1,153 @@
+﻿namespace BasicPlugin
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    using Tekla.Structures.Geometry3d;
+    using Tekla.Structures.Model.UI;
+    using Tekla.Structures.Plugins;
+
+    using TSM = Tekla.Structures.Model;
+
+    /// <summary>
+    /// This class is used for storing data related to the current plugin.
+    /// </summary>
+    public class StructuresData
+    {
+        /// <summary>
+        /// The length factor.
+        /// </summary>
+        [StructuresField("LengthFactor")]
+        public double LengthFactor;
+
+        /// <summary>
+        /// The profile.
+        /// </summary>
+        [StructuresField("Profile")]
+        public string Profile;
+    }
+
+    /// <summary>
+    /// This plugin is similar to the example found in the Open API Reference PluginBase section.
+    /// The plugin asks the user to pick two points. The plug-in then calculates new insertion points
+    /// using a double parameter from the dialog and creates a beam.
+    /// </summary>
+    [Plugin(PluginName)]
+    [PluginUserInterface("BasicPlugin.BeamPluginForm")]
+    public class BeamPlugin : PluginBase
+    {
+        /// <summary>
+        /// Gets the structures data.
+        /// </summary>
+        private StructuresData Data { get; }
+
+        /// <summary>
+        /// The current length factor.
+        /// </summary>
+        private double lengthFactor;
+
+        /// <summary>
+        /// The current profile.
+        /// </summary>
+        private string profile;
+
+        /// <summary>
+        /// The default profile name.
+        /// </summary>
+        public static readonly string DefaultProfileName = "HEA300";
+
+        /// <summary>
+        /// The name of the plugin. This value is used in the attribute above as well as with the Direct Manipulation creation and manipulation features.
+        /// </summary>
+        public const string PluginName = "Beam Plugin Example";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BeamPlugin"/> class.
+        /// </summary>
+        /// <param name="data">The <see cref="StructuresData"/> object for the plugin.</param>
+        public BeamPlugin(StructuresData data)
+        {
+            this.Data = data;
+        }
+
+        /// <inheritdoc />
+        public override bool Run(List<InputDefinition> input)
+        {
+            try
+            {
+                this.GetValuesFromDialog();
+
+                var points = (ArrayList)input[0].GetInput();
+                var point1 = points[0] as Point;
+                var point2 = points[1] as Point;
+                var lengthVector = new Vector(point2 - point1);
+
+                if (this.lengthFactor > 0)
+                {
+                    point2 = this.lengthFactor * lengthVector + point1;
+                }
+
+                this.CreateBeam(point1, point2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(@"Exception: " + ex);
+            }
+
+            return true;
+        }
+
+        /// <inheritdoc />
+        public override List<InputDefinition> DefineInput()
+        {
+            var beamPicker = new Picker();
+            var pointList = new List<InputDefinition>();
+
+            var point1 = beamPicker.PickPoint();
+            var point2 = beamPicker.PickPoint();
+
+            var input1 = new InputDefinition(point1);
+            var input2 = new InputDefinition(point2);
+
+            pointList.Add(input1);
+            pointList.Add(input2);
+
+            return pointList;
+        }
+
+        /// <summary>
+        /// Gets the current values from dialog.
+        /// </summary>
+        private void GetValuesFromDialog()
+        {
+            this.lengthFactor = this.Data.LengthFactor;
+            this.profile = this.Data.Profile;
+            if (this.IsDefaultValue(this.lengthFactor))
+            {
+                this.lengthFactor = 2.0;
+            }
+
+            if (this.IsDefaultValue(this.profile))
+            {
+                this.profile = DefaultProfileName;
+            }
+        }
+
+        /// <summary>
+        /// Creates a beam using two input points.
+        /// </summary>
+        /// <param name="point1">The first input point.</param>
+        /// <param name="point2">The second input point.</param>
+        private void CreateBeam(Point point1, Point point2)
+        {
+            var myBeam = new TSM.Beam(point1, point2)
+            {
+                Profile = { ProfileString = this.profile },
+                Finish = "PAINT"
+            };
+
+            myBeam.Insert();
+        }
+    }
+}
---
+++ DirectManipulationPlugins.Examples_2020/ComplexTransitionSectionPluginFeatures/TransitionSectionManipulationFeature.cs
@@ -0,0 +1,150 @@
+﻿namespace ComplexTransitionSectionPluginFeatures
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Tekla.Structures.Model;
+    using Tekla.Structures.Plugins.DirectManipulation.Core;
+    using Tekla.Structures.Plugins.DirectManipulation.Core.Features;
+    using Tekla.Structures.Plugins.DirectManipulation.Services.Controls;
+
+    using ComplexTransitionSectionPlugin;
+    using ManipulationContexts;
+    using Tekla.Structures;
+
+    /// <summary>
+    /// Direct Manipulation feature for manipulating the transition section plugin.
+    /// </summary>
+    /// <seealso cref="PluginManipulationFeatureBase" />
+    public sealed class TransitionSectionManipulationFeature : PluginManipulationFeatureBase
+    {
+        /// <summary>
+        /// The rectangle height.
+        /// </summary>
+        private double rectangleHeight = TransitionSectionPlugin.DefaultRectangleHeight;
+
+        /// <summary>
+        /// The rectangle width.
+        /// </summary>
+        private double rectangleWidth = TransitionSectionPlugin.DefaultRectangleWidth;
+
+        /// <summary>
+        /// The circle radius.
+        /// </summary>
+        private double circleRadius = TransitionSectionPlugin.DefaultCircleRadius;
+
+        /// <summary>
+        /// Value box that contains the radius of the circular section.
+        /// </summary>
+        private ValueBoxControl radiusValueBox;
+
+        /// <summary>
+        /// Value box that contains the width of the rectangular section.
+        /// </summary>
+        private ValueBoxControl widthValueBox;
+
+        /// <summary>
+        /// Value box that contains the height of the rectangular section.
+        /// </summary>
+        private ValueBoxControl heightValueBox;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TransitionSectionManipulationFeature"/> class.
+        /// </summary>
+        public TransitionSectionManipulationFeature()
+            : base(TransitionSectionPlugin.PluginName, useFeatureContextualToolBar: true)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void DefineFeatureContextualToolbar(IToolbar toolbar)
+        {
+            this.radiusValueBox = toolbar.CreateValueTextBox();
+            this.radiusValueBox.Tooltip = "Top radius";
+            this.radiusValueBox.Title = "r=";
+            this.radiusValueBox.StateChanged += (control, eventArgs) =>
+            {
+                foreach (var component in this.Components)
+                {
+                    this.ModifyComponent(component, TransitionSectionPluginPropertyNames.CircleRadius, this.radiusValueBox.Value);
+                }
+            };
+
+            this.widthValueBox = toolbar.CreateValueTextBox();
+            this.widthValueBox.Tooltip = "Bottom width";
+            this.widthValueBox.Title = "w=";
+            this.widthValueBox.StateChanged += (control, eventArgs) =>
+            {
+                foreach (var component in this.Components)
+                {
+                    this.ModifyComponent(component, TransitionSectionPluginPropertyNames.RectangleWidth, this.widthValueBox.Value);
+                }
+            };
+
+            this.heightValueBox = toolbar.CreateValueTextBox();
+            this.heightValueBox.Tooltip = "Bottom height";
+            this.heightValueBox.Title = "h=";
+            this.heightValueBox.StateChanged += (control, eventArgs) =>
+            {
+                foreach (var component in this.Components)
+                {
+                    this.ModifyComponent(component, TransitionSectionPluginPropertyNames.RectangleHeight, this.heightValueBox.Value);
+                }
+            };
+        }
+
+        /// <inheritdoc />
+        protected override void Refresh()
+        {
+            this.GetCurrentValues(this.Components.First());
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<ManipulationContext> AttachManipulationContexts(Component component)
+        {
+            yield return new RectangleManipulationContext(component, this);
+            yield return new CircleManipulationContext(component, this);
+        }
+
+        /// <summary>
+        /// Gets the current values of the plugin from the associated <see cref="TransitionSectionPluginData"/> data structure,
+        /// and assigns the the values to the correct value text boxes.
+        /// </summary>
+        private void GetCurrentValues(Component component)
+        {
+            if (!(component.Select() && component.GetAttribute(TransitionSectionPluginPropertyNames.RectangleHeight, ref this.rectangleHeight)))
+            {
+                this.rectangleHeight = TransitionSectionPlugin.DefaultRectangleHeight;
+            }
+
+            if (!(component.Select() && component.GetAttribute(TransitionSectionPluginPropertyNames.RectangleWidth, ref this.rectangleWidth)))
+            {
+                this.rectangleWidth = TransitionSectionPlugin.DefaultRectangleWidth;
+            }
+
+            if (!(component.Select() && component.GetAttribute(TransitionSectionPluginPropertyNames.CircleRadius, ref this.circleRadius)))
+            {
+                this.circleRadius = TransitionSectionPlugin.DefaultCircleRadius;
+            }
+
+            this.radiusValueBox.Value = this.circleRadius;
+            this.heightValueBox.Value = this.rectangleHeight;
+            this.widthValueBox.Value = this.rectangleWidth;
+        }
+
+        /// <summary>
+        /// Modifies the component.
+        /// </summary>
+        /// <param name="component">The component.</param>
+        private void ModifyComponent(Component component, string key, double value)
+        {
+            if (component == null || component.Identifier.Equals(new Identifier()) || !component.Select())
+            {
+                return;
+            }
+
+            component.SetAttribute(key, value);
+            component.Modify();
+        }
+    }
+}
