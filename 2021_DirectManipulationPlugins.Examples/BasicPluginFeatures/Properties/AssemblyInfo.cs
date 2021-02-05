240732e97827769b1ee549fdd0982b7d26581469
cahe <carlos.herrero@trimble.com>
2021-02-05 17:28:26 +0200

    Temp. Added 2021 version. Needs some fixes

---
+++ 2021_DirectManipulationPlugins.Examples/BasicPluginFeatures/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BasicPluginFeatures")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Trimble")]
+[assembly: AssemblyProduct("BasicPluginFeatures")]
+[assembly: AssemblyCopyright("Copyright © 1992-2019 Trimble Solutions Corporation and its licensors. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("91f163dc-5f74-44d1-9608-f5c8474dd71b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2020.0.0.0")]
+[assembly: AssemblyFileVersion("2020.0.0.0")]
---
+++ 2021_DirectManipulationPlugins.Examples/ComplexTransitionSectionPlugin/TransitionSectionPluginPropertyNames.cs
@@ -0,0 +1,38 @@
+﻿namespace ComplexTransitionSectionPlugin
+{
+    /// <summary>
+    /// A static class that contains the property names for the plugin data class.
+    /// </summary>
+    public static class TransitionSectionPluginPropertyNames
+    {
+        /// <summary>
+        /// The section profile.
+        /// </summary>
+        public const string SectionProfile = "SectionProfile";
+
+        /// <summary>
+        /// The material.
+        /// </summary>
+        public const string Material = "Material";
+
+        /// <summary>
+        /// The finish.
+        /// </summary>
+        public const string Finish = "Finish";
+
+        /// <summary>
+        /// The rectangle width.
+        /// </summary>
+        public const string RectangleWidth = "RectangleWidth";
+
+        /// <summary>
+        /// The rectangle height.
+        /// </summary>
+        public const string RectangleHeight = "RectangleHeight";
+
+        /// <summary>
+        /// The circle radius.
+        /// </summary>
+        public const string CircleRadius = "CircleRadius";
+    }
+}
